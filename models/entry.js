59f2a49e603fe0f88853298cc810dd0f81e17fe7
Coder_Kartik <kkartiksharma999@gmail.com>
2025-10-04 16:49:30 +0530

    Add files via upload

---
+++ models/entry.js
@@ -0,0 +1,10 @@
+class Entry{
+    constructor(id,date,mood,entry){
+        this.id = id;
+        this.data = date;
+        this.mood = mood;
+        this.entry = entry;
+    }
+}
+
+export { Entry };
